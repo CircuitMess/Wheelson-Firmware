0d1d489a03b75b69945e3f8dcc757806da37c7f4
Filip Budiša <filip@circuitmess.com>
2020-05-28 15:17:34 +0200

    Add bg fleha element

---
+++ src/Elements/Fleha.hpp
@@ -0,0 +1,27 @@
+#ifndef AUTOCAR_FLEHA_H
+#define AUTOCAR_FLEHA_H
+
+#include <UI/Drawable.h>
+
+class Fleha : public Drawable {
+public:
+	Fleha(ElementContainer* parent, uint width, uint height) : Drawable(parent, width, height){ }
+
+	void draw() override {
+		Sprite* canvas = getSprite();
+
+		canvas->fillRect(getX(), getY() + 2, getWidth(), getHeight() - 4, bgColor);
+		canvas->drawFastHLine( getX() + 1, getY(), getWidth() - 2, borderTopColor);
+		canvas->drawFastHLine(getX(), getY() + 1, getWidth(), borderTopColor);
+		canvas->drawFastHLine(getX(), getY() + getHeight() - 2, getWidth(), borderBotColor);
+		canvas->drawFastHLine(getX() + 1, getY() + getHeight() - 1, getWidth() - 2, borderBotColor);
+	}
+
+private:
+	Color bgColor = C_HEX(0x00beff);
+	Color borderTopColor = C_HEX(0x00ffff);
+	Color borderBotColor = C_HEX(0x0082ff);
+
+};
+
+#endif //AUTOCAR_FLEHA_H
