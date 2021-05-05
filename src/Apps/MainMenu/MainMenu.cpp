fa0bc10ea988b1e0719c5cc048fce8961b924940
hrvojekolar <hrvojekolar1998@gmail.com>
2021-05-05 16:30:19 +0200

    Add MainMenu (#4)
    * add SPIFFS pictures in MainMenu
    
    * make new class MainMenu
    
    * new custom element MainMenuApp
    
    * change directory of MainMenu
    
    * Rename MainMenuApp to MainMenuItem
    
    * rename data/MainMenu directory
    
    * add override pack() function in MainMenu
    
    * rename title array AppTitles in MainMenu
    
    * remove drawing border from MainMenu
    
    * change w_malloc to ps_malloc in MainMenu
    
    * remove aditional variable for font in MainMenu
    
    * rename AppTitles array in print in MainMenu
    
    * rename directory of SPIFFS MainMenu
    
    * set initial value for selected app in MainMenu
    
    * add border drawing in MainMenuItem
    
    * replace borderBuffer to setSelected() in MainMenuItem

---
+++ src/Apps/MainMenu/MainMenu.cpp
@@ -0,0 +1,91 @@
+#include "MainMenu.h"
+#include "../Timeline/TimelineApp.h"
+#include <Input/Input.h>
+#include <FS/CompressedFile.h>
+#include <U8g2_for_TFT_eSPI.h>
+
+const char* const MainMenu::AppTitles[] = {"Autonomous", "Simple", "Ball", "Object", "Settings"};
+
+MainMenu* MainMenu::instance = nullptr;
+
+MainMenu::MainMenu(Display& display) : Context(display), layout(&screen){
+	instance = this;
+	buildUI();
+
+	apps[appNum]->setSelected(true);
+
+	MainMenu::pack();
+
+}
+
+MainMenu::~MainMenu(){
+	instance = nullptr;
+}
+
+void MainMenu::start(){
+	draw();
+	screen.commit();
+
+}
+
+
+void MainMenu::stop(){
+
+}
+
+void MainMenu::unpack(){
+	Context::unpack();
+
+	backgroundBuffer = static_cast<Color*>(ps_malloc(160 * 128 * 2));
+	if(backgroundBuffer == nullptr){
+		Serial.printf("MainMenu background picture unpack error\n");
+		return;
+	}
+
+	fs::File backgroundFile = CompressedFile::open(SPIFFS.open("/MainMenu/mainmenu_bg.raw.hs"),13,12);
+
+	backgroundFile.read(reinterpret_cast<uint8_t*>(backgroundBuffer), 160 * 128 * 2);
+	backgroundFile.close();
+
+}
+
+void MainMenu::pack(){
+	Context::pack();
+	free(backgroundBuffer);
+}
+
+void MainMenu::draw(){
+	screen.getSprite()->drawIcon(backgroundBuffer, 0, 0, 160, 128, 1);
+
+	FontWriter u8f = screen.getSprite()->startU8g2Fonts();
+	u8f.setFont(u8g2_font_HelvetiPixel_tr);
+	u8f.setForegroundColor(TFT_WHITE);
+	u8f.setFontMode(1);
+	u8f.setCursor(55, 120);
+	u8f.print(AppTitles[appNum]);
+	screen.draw();
+
+}
+
+void MainMenu::buildUI(){
+	layout.setWHType(PARENT, PARENT);
+	layout.setGutter(5);
+
+	for(int i = 0; i < 5; i++){
+		MainMenuItem* app = new MainMenuItem(&screen, static_cast<MenuApp>(i));
+		apps.push_back(app);
+		layout.addChild(app);
+	}
+	apps[0]->setX(10);apps[0]->setY(15);
+	apps[1]->setX(60);apps[1]->setY(15);
+	apps[2]->setX(110);apps[2]->setY(15);
+	apps[3]->setX(40);apps[3]->setY(65);
+	apps[4]->setX(90);apps[4]->setY(65);
+	layout.reflow();
+	screen.addChild(&layout);
+	screen.repos();
+
+
+}
+
+
