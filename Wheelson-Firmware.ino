1d74cb21441721ced46812efde48af2eef854858
Filip Budiša <filip@circuitmess.com>
2021-05-05 16:51:55 +0200

    Clean up .ino file

--- Wheelson-Firmware.ino
+++ Wheelson-Firmware.ino
@@ -1,73 +1,34 @@
 #include <Arduino.h>
 #include <CircuitOS.h>
-#include <Support/Context.h>
 #include <Loop/LoopManager.h>
-#include <Input/InputGPIO.h>
-#include <Input/InputI2C.h>
-#include "src/Apps/MainMenu/MainMenu.h"
-#include <Sync/Mutex.h>
-#include "src/Components/Motors.h"
-#include "src/Apps/AutonomousDriving/autonomousSettings.h"
 #include <esp32-hal-psram.h>
-//#include <NeoPixelBus.h>
-//#include <NeoPixelAnimator.h>
-//#include <NeoPixelBrightnessBus.h>
-#define CAMERA_MODEL_AI_THINKER
-#include "src/Components/ActionProcessor.h"
+#include <Display/Display.h>
+#include <SPIFFS.h>
+#include "src/IntroScreen.h"
 
-//#include <NeoPixelBus.h>
-//#include <NeoPixelAnimator.h>
-//#include <NeoPixelBrightnessBus.h>
-
-#define blPin 25
 
 Display display(160, 128, -1, -1);
-InputI2C* input = nullptr;
-I2cExpander i2c;
-Context* menu = nullptr;
-Motors motors(&i2c);
-const uint16_t PixelCount = 4; // the sample images are meant for 144 pixels
-const uint16_t PixelPin = 32;
-const uint16_t AnimCount = 200;
-//NeoPixelBrightnessBus<NeoGrbFeature, Neo800KbpsMethod> strip(PixelCount, PixelPin);
 
 void setup(){
 	Serial.begin(115200);
-	//strip.Begin();
-//	strip.ClearTo(RgbColor(255, 255, 255));
-	//strip.SetBrightness(255);
-	//strip.Show();
+
+	if(psramFound()){
+		Serial.printf("PSRAM init: %s, free: %d B\n", psramInit() ? "Yes" : "No", ESP.getFreePsram());
+	}else{
+		Serial.println("No PSRAM");
+	}
+
 	if(!SPIFFS.begin()){
 		Serial.println("SPIFFS error");
 	}
-	psramInit();
-	Serial.print("PSRAM found: ");
-	Serial.println(psramFound());
 
-	pinMode(blPin, OUTPUT);
-	digitalWrite(blPin, LOW);
 	display.begin();
 
-	Settings::init(new SettingsStruct, sizeof(SettingsStruct));
-	//JayD.begin();
-	ActionProcessor* processor = new ActionProcessor();
-//	i2c.begin(I2C_EXPANDER_ADDRESS, 14, 15);
-	i2c.pinMode(14, OUTPUT);
-	i2c.pinWrite(14, 0);
-	motors.begin(11, 10, 12, 13);
-	input = new InputI2C(&i2c);
-
-	menu = new MainMenu(display);
-	menu->unpack();
-	menu->start();
-
-	Task::setPinned(true);
-	LoopManager::addListener(input);
-	LoopManager::startTask();
-
-	vTaskDelete(NULL);
+	IntroScreen::IntroScreen* intro = new IntroScreen::IntroScreen(display);
+	intro->unpack();
+	intro->start();
 }
 
 void loop(){
-	//LoopManager::loop();
+	LoopManager::loop();
 }
