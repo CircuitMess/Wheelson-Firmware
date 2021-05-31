17c1a334834eed44a4848eb39e3d1164066b7dbf
hrvojekolar <hrvojekolar1998@gmail.com>
2021-05-31 16:59:33 +0200

    Autonomous screen (#9)

---
+++ src/Apps/AutonomousDrivingApp/AutonomousDriving.h
@@ -0,0 +1,43 @@
+#ifndef WHEELSON_FIRMWARE_AUTONOMOUSDRIVING_H
+#define WHEELSON_FIRMWARE_AUTONOMOUSDRIVING_H
+
+#include <Arduino.h>
+#include <Support/Context.h>
+#include <UI/Layout.h>
+#include <Elements/GridMenu.h>
+#include <Input/InputListener.h>
+#include <Loop/LoopListener.h>
+#include "DrivingElement.h"
+
+class AutonomousDriving : public Context, public LoopListener {
+public:
+	AutonomousDriving(Display& display);
+
+	virtual ~AutonomousDriving();
+
+	void start() override;
+
+	void stop() override;
+
+	void draw() override;
+
+	void loop(uint micros) override;
+
+protected:
+	void init() override;
+
+	void deinit() override;
+
+private:
+	static AutonomousDriving* instance;
+
+	LinearLayout* screenLayout;
+	std::vector<DrivingElement*> engines;
+	Color* backgroundBuffer = nullptr;
+	Color* cameraBuffer = nullptr;
+
+	void buildUI();
+
+};
+
+#endif //WHEELSON_FIRMWARE_AUTONOMOUSDRIVING_H
