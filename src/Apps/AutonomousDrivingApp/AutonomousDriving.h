168f0b687e3c4e73ace689f56749747dbb2fe899
Filip Budiša <filip@circuitmess.com>
2021-06-02 09:22:03 +0200

    Put Driver processing in separate task

--- src/Apps/AutonomousDrivingApp/AutonomousDriving.h
+++ src/Apps/AutonomousDrivingApp/AutonomousDriving.h
@@ -35,7 +35,7 @@ private:
 	LinearLayout* screenLayout;
 	std::vector<DrivingElement*> engines;
 	Color* backgroundBuffer = nullptr;
-	Color* cameraBuffer = nullptr;
+	const Color* cameraBuffer = nullptr;
 	Driver* driver;
 	uint8_t motorPower[4] = {0};
 	char buffer[4];
--- src/Components/Driver.h
+++ src/Components/Driver.h
@@ -3,23 +3,32 @@
 
 #include <Loop/LoopListener.h>
 #include <Display/Color.h>
+#include <Util/Task.h>
 #include "CameraFeed.h"
 
-class Driver : public LoopListener {
+class Driver {
 public:
 	Driver();
+	void start();
+	void stop();
 
-	virtual uint16_t* getCameraImage();
 	virtual uint getMotorState(uint id);
-	virtual void start() = 0;
-	virtual void stop() = 0;
+	const Color* getCameraImage();
 
-	void loop(uint micros) override;
+	bool isRunning() const;
+
+	static void taskFunc(Task* task);
+
+protected:
+	virtual void process() = 0;
+	void setMotor(uint8_t id, int8_t state);
 
 private:
-protected:
+	CameraFeed cam;
 	Color* frameBuffer = nullptr;
+	int8_t motors[4] = {0};
 
+	Task task;
 };
 
 #endif //WHEELSON_FIRMWARE_DRIVER_HPP
