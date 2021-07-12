4b9742e842d8aea3d01f9f69f06a0c907d65135a
hrvojekolar <hrvojekolar1998@gmail.com>
2021-07-12 17:35:14 +0200

    add cameraFeed check in CameraHWTest

--- src/UserHWTest/CameraHWTest.cpp
+++ src/UserHWTest/CameraHWTest.cpp
@@ -18,10 +18,15 @@ void CameraHWTest::draw(){
 	u8f.setFont(u8g2_font_HelvetiPixel_tr);
 	u8f.setForegroundColor(TFT_WHITE);
 	u8f.setFontMode(1);
-	u8f.setCursor((160 - u8f.getUTF8Width("If camera works good")) / 2, 53);
-	u8f.print("If camera works good");
-	u8f.setCursor((130 - u8f.getUTF8Width("press any button")) / 2, 63);
-	u8f.print("    press any button");
+	if(!cameraFeed.isInited()){
+		u8f.setCursor((160 - u8f.getUTF8Width("Camera is not detected!")) / 2, 53);
+		u8f.print("Camera is not detected!");
+	}else{
+		u8f.setCursor((160 - u8f.getUTF8Width("If camera works good")) / 2, 53);
+		u8f.print("If camera works good");
+		u8f.setCursor((130 - u8f.getUTF8Width("press any button")) / 2, 63);
+		u8f.print("    press any button");
+	}
 }
 
 void CameraHWTest::start(){
